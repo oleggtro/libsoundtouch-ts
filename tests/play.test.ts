d22b2cd7bc01505273d427342a146d90a5eebfdd
Ole <ole@oleggtro.com>
2025-10-30 17:19:04 +0100

    tests: more test for playURL

--- tests/play.test.ts
+++ tests/play.test.ts
@@ -82,5 +82,75 @@ describe("SoundTouchAPI /speaker endpoint", () => {
 
     expect(playURLResponse).toBe("/speaker");
     }); 
+
+
+       it("parses /speaker current volume 2 response correctly", async () => {
+
+    const expected_request = `<play_info>
+  <url>http://www.hyperion-records.co.uk/audiotest/14%20Clementi%20Piano%20Sonata%20in%20D%20major,%20Op%2025%20No%206%20-%20Movement%202%20Un%20poco%20andante.MP3</url>
+  <app_key>Xp7YGBI9dh763Kj8sY8e86JPXtisddBa</app_key>
+  <service>Clementi</service>
+  <message>Movements Album</message>
+  <reason>Piano Sonata in D major</reason>
+</play_info>`;
+
+
+    nock(baseURL)
+      .post("/speaker", (body) => body.replace(/\s+/g, "") === expected_request.replace(/\s+/g, ""))
+      .matchHeader("Content-Type", "application/xml")
+      .reply(200, mockXML_playURLResponse, { "Content-Type": "application/xml" });
+
+    const request = {
+      url: "http://www.hyperion-records.co.uk/audiotest/14%20Clementi%20Piano%20Sonata%20in%20D%20major,%20Op%2025%20No%206%20-%20Movement%202%20Un%20poco%20andante.MP3",
+      app_key: "Xp7YGBI9dh763Kj8sY8e86JPXtisddBa",
+      service: "Clementi",
+      message: "Movements Album",
+      reason: "Piano Sonata in D major",
+    };
+
+
+    const playURLResponse = await api.playURL(request);
+    // Ensure the mocked endpoint was called
+    expect(nock.isDone()).toBe(true);
+
+    expect(playURLResponse).toBe("/speaker");
+    }); 
+
+    
+it("parses /speaker current volume 2 response correctly", async () => {
+
+    const expected_request = `<play_info>
+  <url>http://www.hyperion-records.co.uk/audiotest/14%20Clementi%20Piano%20Sonata%20in%20D%20major,%20Op%2025%20No%206%20-%20Movement%202%20Un%20poco%20andante.MP3</url>
+  <app_key>Xp7YGBI9dh763Kj8sY8e86JPXtisddBa</app_key>
+  <service>Clementi</service>
+  <message>Movements Album</message>
+  <reason>Piano Sonata in D major</reason>
+  <volume>50</volume>
+</play_info>`;
+
+
+    nock(baseURL)
+      .post("/speaker", (body) => body.replace(/\s+/g, "") === expected_request.replace(/\s+/g, ""))
+      .matchHeader("Content-Type", "application/xml")
+      .reply(200, mockXML_playURLResponse, { "Content-Type": "application/xml" });
+
+    const request = {
+      url: "http://www.hyperion-records.co.uk/audiotest/14%20Clementi%20Piano%20Sonata%20in%20D%20major,%20Op%2025%20No%206%20-%20Movement%202%20Un%20poco%20andante.MP3",
+      app_key: "Xp7YGBI9dh763Kj8sY8e86JPXtisddBa",
+      service: "Clementi",
+      message: "Movements Album",
+      reason: "Piano Sonata in D major",
+      volume: 50,
+    };
+
+
+    const playURLResponse = await api.playURL(request);
+    // Ensure the mocked endpoint was called
+    expect(nock.isDone()).toBe(true);
+
+    expect(playURLResponse).toBe("/speaker");
+    }); 
+
+
 });
 
